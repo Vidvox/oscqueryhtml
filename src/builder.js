0062bf42314e4506d271dc923fb36e4082e78adf
Dustin Long <me@dustmop.io>
2018-03-26 18:55:35 -0400

    Additional html controls

--- src/builder.js
+++ src/builder.js
@@ -5,39 +5,59 @@ function buildSingleControl(name, details) {
     html += '<span class="control-name">' + name + '</span>';
     html += '<span class="description">' + details.DESCRIPTION + '</span>';
     if (details.TYPE == 'c') {
-        html += '<span class="type">TODO: char node</span>';
+        // Char
+        html += '<input type="text" maxlength="1" size="3"/>';
     } else if (details.TYPE == 'r') {
+        // Color
         // TODO: Third-party color picker.
         html += '<span class="type">TODO: color node</span>';
+        console.log('========================================');
+        console.log(details);
     } else if (details.TYPE == 'd') {
+        // Double
         let min = details.RANGE[0].MIN;
         let max = details.RANGE[0].MAX;
         html += '<input type="range" min="' + min + '" max="' + max + '"/>';
     } else if (details.TYPE == 'F') {
+        // False
         html += '<input type="button" value="Send false"/>';
     } else if (details.TYPE == 'f') {
+        // Float
         let min = details.RANGE[0].MIN;
         let max = details.RANGE[0].MAX;
         html += '<input type="range" min="' + min + '" max="' + max + '"/>';
     } else if (details.TYPE == 'I') {
+        // Infinity
         html += '<input type="button" value="Send infinity"/>';
     } else if (details.TYPE == 'i') {
-        // TODO: Text box.
-        html += '<span class="type">TODO: int node</span>';
+        // Integer
+        let min = details.RANGE[0].MIN;
+        let max = details.RANGE[0].MAX;
+        html += '<input type="range" min="' + min + '" max="' + max + '"/>';
     } else if (details.TYPE == 'h') {
-        html += '<span class="type">TODO: longlong node</span>';
+        // Longlong
+        let min = details.RANGE[0].MIN;
+        let max = details.RANGE[0].MAX;
+        html += '<input type="range" min="' + min + '" max="' + max + '"/>';
     } else if (details.TYPE == 'm') {
+        // Midi
         html += '<span class="type">TODO: midi node</span>';
+        console.log('========================================');
+        console.log(details);
     } else if (details.TYPE == 'N') {
+        // Null
         html += '<input type="button" value="Send null"/>';
     } else if (details.TYPE == 's') {
-        // TODO: Text box.
-        html += '<span class="type">TODO: string node</span>';
+        // String
+        html += '<input type="text"/>';
     } else if (details.TYPE == 'T') {
+        // True
         html += '<input type="button" value="Send true"/>';
     } else if (details.TYPE == 't') {
-        // TODO: ?????.
+        // Timetag
         html += '<span class="type">TODO: timetag node</span>';
+        console.log('========================================');
+        console.log(details);
     } else {
         html += '<span class="type">UNKNOWN (' + details.TYPE + ')</span>';
     }
