87fc1665d5dc95289d2da1364dd778d8bf14ea1f
Dustin Long <me@dustmop.io>
2018-03-26 15:01:13 -0400

    Iterate over the controls within the json

--- src/retrieve.js
+++ src/retrieve.js
@@ -12,11 +12,25 @@ function retrieveJson(url, cb) {
     });
 }
 
+function objectGetValue(obj, i) {
+    return obj[Object.keys(obj)[i]];
+}
+
 function main() {
     const url = process.env['SERVER_URL'];
     retrieveJson(url, (result) => {
-        console.log(JSON.stringify(result, null, 2));
+        let keys = Object.keys(result);
+        console.log(keys);
+        let contents = result.CONTENTS;
+        // TODO: Iterate all directories?
+        let dir = objectGetValue(contents, 0);
+        let innerContents = dir.CONTENTS;
+        console.log(Object.keys(innerContents));
+        for (let nodeKey in innerContents) {
+            let node = innerContents[nodeKey];
+            let nodeType = node.TYPE;
+            console.log(node['FULL_PATH'] + ' ' + nodeType);
+        }
     });
 }
 main();
-
