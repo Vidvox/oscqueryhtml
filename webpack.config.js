510cf4c580186253040b22b23aee47b3f9b0f1d2
Dustin Long <me@dustmop.io>
2018-04-23 17:17:21 -0400

    Enable source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
