d3b283eb34c55dbc9bdce915f0e5f67be0cfec5a
Dustin Long <me@dustmop.io>
2018-07-17 22:23:52 -0400

    Basic unit testing using mocha

---
+++ test/builder.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+describe('Basic', () => {
+    describe('math', () => {
+        it('should add correctly', () => {
+            assert.equal(1 + 1, 2);
+        });
+    });
+});
